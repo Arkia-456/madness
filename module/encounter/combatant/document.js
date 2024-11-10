b66f1a0b8ea7c45258453449a66c450318096acb
Arkia <145221230+Arkia-456@users.noreply.github.com>
2024-11-10 20:42:39 +0100

    Encounter: Log actor recap for GM at actor's turn start

--- module/encounter/combatant/document.js
+++ module/encounter/combatant/document.js
@@ -1,11 +1,51 @@
+import { ChatMessageMadness } from '../../chat-message/index.js';
+
 class CombatantMadness extends Combatant {
 	endTurn() {
 		this._applyDoT(this.actor, 'end');
 		this._removeBuffsAndDebuffs(this.actor, 'end');
 	}
 
-	startTurn() {
-		this._removeBuffsAndDebuffs(this.actor, 'start');
+	async startTurn() {
+		await this._removeBuffsAndDebuffs(this.actor, 'start');
+		this._logRecap();
+	}
+
+	async _logRecap() {
+		const actor = this.token.actor;
+		const passives = actor.effects.map((e) => {
+			return {
+				name: e.name,
+				stacks: e.system.stacks,
+				duration: e.system.durations?.reduce(
+					(acc, d) => (acc += `${d.value} ${d.type}`),
+					'',
+				),
+			};
+		});
+		const templateData = {
+			hp: actor.system.hp.value,
+			hpmax: actor.system.hp.max,
+			hptemp: actor.system.hp.temp,
+			mp: actor.system.mp.value,
+			mpmax: actor.system.mp.max,
+			armor: actor.system.armor,
+			movement: actor.system.secondaryAttributes.maxMoveDistance.total,
+			passives,
+		};
+		const template = 'systems/madness/templates/chat/recap-card.hbs';
+		const chatData = {
+			speaker: ChatMessageMadness.getSpeaker({
+				actor,
+				token: this.token,
+			}),
+			whisper: game.users.reduce((users, u) => {
+				if (u.isGM) users.push(u.id);
+				return users;
+			}, []),
+			content: await renderTemplate(template, templateData),
+		};
+		ChatMessageMadness.create(chatData);
 	}
 
 	async _removeBuffsAndDebuffs(actor, applicationTime) {
